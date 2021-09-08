e34447758171d3cfe05ab1ad02ba20adf57fc94a
JohannesFinsveen <56305961+JohannesFinsveen@users.noreply.github.com>
2021-09-08 16:37:31 +0200

    These are not added to the solution, just as a draft on how it could be done

---
+++ PCAxis.Common.UnitTest/SerializerTest.vb
@@ -0,0 +1,47 @@
+Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports PCAxis.Paxiom
+
+
+
+Namespace PCAxis.Common.UnitTest
+    <TestClass>
+    Public Class SerializerTest
+        <TestMethod>
+        Sub NoExceptiongFromCsv2WhenNoContentVariable()
+            Dim builder As PXFileBuilder = New PXFileBuilder()
+
+            'builder.SetPath("C:\github.com\statisticssweden\nugets\PCAxis.Common\PCAxis.Common.UnitTest\PxFiles\officialstatistics.px")
+            builder.SetPath("C:\github.com\statisticssweden\nugets\PCAxis.Common\PCAxis.Common.UnitTest\PxFiles\PR0101B3.px")
+
+            builder.BuildForSelection()
+            Dim selections As Selection() = Selection.SelectAll(builder.Model.Meta)
+            builder.BuildForPresentation(selections)
+            Dim model As PXModel = builder.Model
+
+            Dim serializer As IPXModelStreamSerializer = New Csv2FileSerializer()
+
+            Dim excepted = "This is a don't die test, so..."
+
+            Dim actual As String
+            Using ms As New System.IO.MemoryStream
+                serializer.Serialize(model, ms)
+                ms.Position = 0
+
+                Dim sr As New IO.StreamReader(ms)
+                actual = sr.ReadToEnd()
+                Console.WriteLine(actual)
+            End Using
+
+
+
+            'Assert  :-)
+            actual = excepted
+
+            Assert.AreEqual(actual, excepted)
+
+        End Sub
+    End Class
+
+End Namespace
+
